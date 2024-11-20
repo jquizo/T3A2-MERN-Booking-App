5678190ea2a3c260042edfe01663fefe2df23bf6
jquizo <45918580+jquizo@users.noreply.github.com>
2024-11-20 23:55:00 +1100

    added footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div className="bg-orange-700 py-10">
+            <div className="flex justify-between items-center mx-auto container">
+                <span className="text-3xl text-white font-bold tracking-tight">BookMe</span>
+                <span className="flex gap-4 text-white font-bold tracking-tight">
+                    <p className="cursor-pointer">Terms of Service</p>
+                </span>
+            </div>
+        </div>
+    )
+};
+
+export default Footer;
