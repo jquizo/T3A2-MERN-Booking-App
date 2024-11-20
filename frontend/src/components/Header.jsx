7472f0d44dc29d488fc27523201c2d82b4cd9ea7
jquizo <45918580+jquizo@users.noreply.github.com>
2024-11-20 23:24:13 +1100

    added header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,7 +8,14 @@ const Header = () => {
                 <span className="text-white font-bold text-3xl tracking-tight">
                     <Link to="/">BookMe</Link>
                 </span>
+                <span className="flex space-x-2 ">
+                    <Link to="/sign-in" className="flex items-center bg-white text-blue-800 px-3 rounded hover:bg-gray-100">
+                    Sign In
+                    </Link>
+                </span>
             </div>
         </div>
     )
 }
+
+export default Header;
