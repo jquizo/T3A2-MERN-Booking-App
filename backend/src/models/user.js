2bd13c13dbe54a017e80b1ee8a11552799163270
jquizo <45918580+jquizo@users.noreply.github.com>
2024-11-24 21:26:56 +1100

    added comments to code

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from 'bcryptjs';
 
+// Schema for the user collection in database
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
@@ -9,6 +10,7 @@ const userSchema = new mongoose.Schema({
 
 });
 
+// Middleware function for hashing password before saving it to the database
 userSchema.pre("save", async function(next){
     if(this.isModified('password')){
         this.password = await bcrypt.hash(this.password, 8)
@@ -16,6 +18,7 @@ userSchema.pre("save", async function(next){
     next();
 }); 
 
+// Create the model "User" using userSchema and assign it to the User variable
 const User = mongoose.model<UserType>("User", userSchema);
 
 export default User
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,21 +1,49 @@
 import express from 'express';
 import User from '../models/user'
+import jwt from "jsonwebtoken";
 
+// Create express router instance
 const router = express.Router();
 
+// User registration POST route
 router.post("/register", async(req,res) => {
     try {
+        // Checks if email is already in database
         let user = await User.findOne({
             email: req.body.email,
         });
-
+        // If user is already in database, send message "User already exists"
         if(user) {
             return res.status(400).json({ message: "User already exists"});
         }
 
+        // If User does not exist, create user instance using data from request body
         user = new User(req.body)
+        // Then save the user to the database
         await user.save();
+
+        // Generates a JSON web token for the user
+        const token = jwt.sign(
+        {
+            userId: user.id
+        }, 
+            process.env.JWT_SECRET_KEY,
+        { 
+            expiresIn:"2d"
+        }
+        );
+
+        // Sets the cookie to HTTP only
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            // Sends a HTTPS cookie only in production
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 172800000,
+        })
+        return res.sendStatus(200);
     } catch(error) {
         res.status(500).send({ message: "Something went wrong"})
     }
 })
+
+export default router;
