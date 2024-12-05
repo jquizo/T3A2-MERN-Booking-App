d2b7bd2f428a53532afd3a8e472043f127e57bde
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-05 23:47:15 +1100

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
-import userRoutes from './routes/users'
+import userRoutes from './routes/users.js';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
