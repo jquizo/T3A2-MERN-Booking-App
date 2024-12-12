b0c47f82bfb0fb17c5635a6573fd8b136505bdea
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-12 19:21:57 +1100

    created register page

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <form className="flex flex-col gap-4">
+            <h2 className="text-3xl font-bold">Register an account</h2>
+        </form>
+    );
+};
+
+export default Register;
