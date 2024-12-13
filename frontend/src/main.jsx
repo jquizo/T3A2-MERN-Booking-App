2af3f52efe2eb9ddb04d78fb75c4036a1ce3abdf
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-13 21:52:09 +1100

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { queryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 0,
-    },
-  },
-});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </StrictMode>
 );
